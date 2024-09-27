ef483a418ac9d933f2eec7385287045ac4f40402
Quang <quangng263@gmail.com>
2024-09-27 12:20:21 +0700

    update

--- index.js
+++ index.js
@@ -1,164 +1,93 @@
-let arr_input = [];
-let newArr = [];
+let numberArray = [];
 
-document.getElementById("btnThemSo").onclick = function () {
-  let item = document.getElementById("inputSo").value * 1;
-  arr_input.push(item);
-  newArr.push(item);
-  document.getElementById("arrayDisplay").innerHTML = `
-  <h6>${arr_input}</h6>`;
-};
-
-function getNum(arrNum, pos_type = true) {
-  let arrPos = arrNum.filter(function (number) {
-    if (pos_type) {
-      return number > 0;
-    } else {
-      return number < 0;
-    }
-  });
-  return arrPos;
-}
-
-function sortMinToMax(arrNum) {
-  let sortedArr = arrNum.sort(function (a, b) {
-    return a - b;
-  });
-  return sortedArr;
-}
-
-function checkPrimeNumber(number) {
-  let count = 0;
-  if (number < 1) {
-    return false;
+function addNumber() {
+  const number = parseInt(document.getElementById("numberInput").value);
+  if (!isNaN(number)) {
+    numberArray.push(number);
+    document.getElementById("arrayOutput").innerText =
+      "Mảng: " + numberArray.join(", ");
   }
-  for (let i = 1; i < number; i++) {
-    if (number % i == 0) {
-      count += 1;
-    }
-  }
-  return count == 1;
 }
 
-function getInteger(arrNum) {
-  let arrIn = arrNum.filter(function (number) {
-    return number % 1 == 0;
-  });
-  return arrIn;
+function resetArray() {
+  numberArray = [];
+  document.getElementById("arrayOutput").innerText = "Mảng đã được xóa.";
+  document.getElementById("resultOutput").innerText = "";
 }
 
-function sumPosNum(arrPos) {
-  let sum = 0;
-  for (number of arrPos) {
-    sum += number;
-  }
-  return sum;
+function sumPositive() {
+  const sum = numberArray.filter((n) => n > 0).reduce((a, b) => a + b, 0);
+  document.getElementById("resultOutput").innerText = "Tổng số dương: " + sum;
 }
 
-document.getElementById("tinhTong").onclick = function () {
-  let sample_input = [...arr_input];
-  let posArr = getNum(sample_input);
-  let sum = sumPosNum(posArr);
-  document.getElementById("hienThiTong").innerHTML = ` <h6>${sum}</h6>`;
-};
-
-document.getElementById("demSo").onclick = function () {
-  let sample_input = [...arr_input];
-  let posArr = getNum(sample_input);
-  document.getElementById(
-    "hienThiTongSoDuong"
-  ).innerHTML = `<h6>${posArr.length}</h6> `;
-};
-
-document.getElementById("timMin").onclick = function () {
-  let sample_input = [...arr_input];
-  let min = sortMinToMax(sample_input)[0];
-  document.getElementById("hienThiSoNhoNhat").innerHTML = `<h6>${min}</h6>`;
-};
-
-document.getElementById("timDuongNhoNhat").onclick = function () {
-  let sample_input = [...arr_input];
-  let min = sortMinToMax(getNum(sample_input))[0];
-  document.getElementById(
-    "hienThiSoDuongNhoNhat"
-  ).innerHTML = `<h6>${min}</h6>`;
-};
-
-function lastEvenNum(arrNum) {
-  for (let i = arrNum.length - 1; i >= 0; i--) {
-    if (arrNum[i] % 2 == 0) {
-      return arrNum[i];
-    }
-  }
-  return -1;
+function countPositive() {
+  const count = numberArray.filter((n) => n > 0).length;
+  document.getElementById("resultOutput").innerText = "Đếm số dương: " + count;
 }
-document.getElementById("timSoChan").onclick = function () {
-  let sample_input = [...arr_input];
-  let even_num = lastEvenNum(sample_input);
-  document.getElementById("hienThiSoChan").innerHTML = `<h6>${even_num}</h6>`;
-};
 
-function swapNum(arrNum, position1, position2) {
-  let temp = arrNum[position2];
-  arrNum[position2] = arrNum[position1];
-  arrNum[position1] = temp;
-  return arrNum;
+function findMin() {
+  const min = Math.min(...numberArray);
+  document.getElementById("resultOutput").innerText = "Số nhỏ nhất: " + min;
 }
-document.getElementById("swap").onclick = function () {
-  let sample_input = [...arr_input];
-  let pos1 = document.getElementById("soThu1").value * 1;
-  let pos2 = document.getElementById("soThu2").value * 1;
-  let newArr = swapNum(sample_input, pos1, pos2);
-  document.getElementById("hienThiChuoiMoi").innerHTML = `<h6>${newArray}</h6>`;
-};
 
-document.getElementById("sapXep").onclick = function () {
-  let sample_input = [...arr_input];
-  let sortedArr = sortMinToMax(sample_input);
-  document.getElementById(
-    "hienThiChuoiTang"
-  ).innerHTML = `<h6>${sortedArr}</h6>`;
-};
-
-function firstPrimeNum(arrNum) {
-  for (index in arrNum) {
-    if (checkPrimeNumber(arrNum[index])) {
-      return arrNum[index];
-    }
-  }
-  return -1;
+function findMinPositive() {
+  const positives = numberArray.filter((n) => n > 0);
+  const minPositive = Math.min(...positives);
+  document.getElementById("resultOutput").innerText =
+    "Số dương nhỏ nhất: " +
+    (minPositive !== Infinity ? minPositive : " Không có số dương");
 }
-document.getElementById("timSoNguyenTo").onclick = function () {
-  let sample_input = [...arr_input];
-  let num = firstPrimeNum(sample_input);
-  document.getElementById("hienThiSoNguyenTo").innerHTML = `<h6>${num}</h6>`;
-};
 
-document.getElementById("themSoThuc").onclick = function () {
-  let item = document.getElementById("inputSoThuc").value * 1;
-  newArr.push(item);
-  document.getElementById("hienThiNguyenThuc").innerHTML = `<h6>${newArr}</h6>`;
-};
+function findLastEven() {
+  const evens = numberArray.filter((n) => n % 2 === 0);
+  const lastEven = evens.length ? evens[evens.length - 1] : "Không có số chẵn";
+  document.getElementById("resultOutput").innerText =
+    "Số chẵn cuối cùng: " + lastEven;
+}
 
-document.getElementById("demSoNguyen").onclick = function () {
-  let num = getInteger(newArr).length;
-  document.getElementById("hienThiSoNguyen").innerHTML = `<h6>${num}</h6>`;
-};
-
-function comparePosNa(arrNum) {
-  let numPos = getNum(arrNum).length;
-  let numNe = getNum(arrNum, false).length;
-  if (numPos > numNe) {
-    return "So luong so duong lon hon";
-  } else if (numPos < numNe) {
-    return "So luong so am lon hon";
+function swapPositions() {
+  if (numberArray.length >= 2) {
+    [numberArray[0], numberArray[numberArray.length - 1]] = [
+      numberArray[numberArray.length - 1],
+      numberArray[0],
+    ];
+    document.getElementById("resultOutput").innerText =
+      "Mảng sau khi đổi chỗ: " + numberArray.join(", ");
   } else {
-    return "So luong so am va so duong bang nhau";
+    document.getElementById("resultOutput").innerText =
+      "Cần ít nhất 2 số để đổi chỗ. ";
   }
 }
-document.getElementById("soSanh").onclick = function () {
-  let sample_input = [...arr_input];
-  document.getElementById("hienThiThongTin").innerHTML = `<h6>${comparePosNa(
-    sample_input
-  )}</h6>`;
-};
+
+function sortArray() {
+  numberArray.sort((a, b) => a - b);
+  document.getElementById("resultOutput").innerText =
+    "Mảng sau khi sắp xếp tăng dần: " + numberArray.join(", ");
+}
+
+function findFirstPrime() {
+  function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+  const firstPrime = numberArray.find(isPrime);
+  document.getElementById("resultOutput").innerText =
+    "Số nguyên tố đầu tiên: " +
+    (firstPrime !== undefined ? firstPrime : "Không có số nguyên tố");
+}
+
+function countIntegers() {
+  const count = numberArray.length;
+  document.getElementById("resultOutput").innerText = "Đếm số nguyên: " + count;
+}
+
+function compareNegativePositive() {
+  const countPositive = numberArray.filter((n) => n > 0).length;
+  const countNegative = numberArray.filter((n) => n < 0).length;
+  document.getElementById(
+    "resultOutput"
+  ).innerText = `Số dương: ${countPositive}, Số âm: ${countNegative}`;
+}
